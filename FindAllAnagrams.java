feb087a1a251f365113e58081cebc78f57ba239f
Vanshika Gupta <vanshikagupta0009@gmail.com>
2025-08-30 11:05:49 +0530

    commit

---
+++ FindAllAnagrams.java
@@ -0,0 +1,33 @@
+package SlidingWindow;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllAnagrams {
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] freqP = new int[26];
+
+        for(int i = 0; i < p.length(); i++){
+            freqP[p.charAt(i) - 'a']++;
+        }
+
+        int left = 0;
+        int[] freqS = new int[26];
+        List<Integer> result = new ArrayList<>();
+        int index = 0;
+        for(int right = 0; right < s.length(); right++){
+            freqS[s.charAt(right) - 'a']++;
+
+            if((right - left + 1) > p.length()){
+                freqS[s.charAt(left) - 'a']--;
+                left++;
+            }
+
+            if(Arrays.equals(freqS, freqP)){
+                result.add(left);
+            }
+        }
+        return result;
+    }
+}
